0f781a03a73b50365d7c233bb0bb00eed9a669d9
topklc <197289236+topklc@users.noreply.github.com>
2026-08-09 20:37:59 +0300

    added official caddy repo

--- config/setup.sh
+++ config/setup.sh
@@ -11,7 +11,13 @@ apt update
 apt upgrade -y
 
 # install
-apt install -y sudo caddy fail2ban ufw git unattended-upgrades knot
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+sudo chmod o+r /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install -y caddy fail2ban ufw git unattended-upgrades knot
 
 # firewall
 ufw default deny incoming
