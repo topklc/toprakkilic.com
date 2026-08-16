747818f54ee036a00d782123b296576d2cc38d8d
topklc <197289236+topklc@users.noreply.github.com>
2026-08-16 18:24:23 +0300

    Added hardening file, reorganized/updated setup.sh, tidied up zone, and increased bar length to 100%

--- config/setup.sh
+++ config/setup.sh
@@ -9,24 +9,27 @@ ssh-keygen -lf /dev/stdin <<<"$PUBKEY" >/dev/null || { echo "public key not vali
 id admin &>/dev/null || adduser admin
 usermod -aG sudo admin
 
-# software
+# installing
 
 ## caddy setup
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 chmod o+r /etc/apt/sources.list.d/caddy-stable.list
 
 ## knot setup
 wget -O /usr/share/keyrings/cznic-labs-pkg.gpg https://pkg.labs.nic.cz/gpg
-echo "deb [signed-by=/usr/share/keyrings/cznic-labs-pkg.gpg] https://pkg.labs.nic.cz/knot-dns trixie main" | sudo tee /etc/apt/sources.list.d/cznic-labs-knot-dns.list
+echo "deb [signed-by=/usr/share/keyrings/cznic-labs-pkg.gpg] https://pkg.labs.nic.cz/knot-dns trixie main" | tee /etc/apt/sources.list.d/cznic-labs-knot-dns.list
 
 ## software
 apt update
 apt upgrade -y
 apt install -y git caddy fail2ban ufw unattended-upgrades knot knot-dnssecutils knot-dnsutils knot-keymgr debian-keyring debian-archive-keyring apt-transport-https ca-certificates fastfetch
+[ -d /srv/www/toprakkilic.com/.git ] || git clone https://github.com/topklc/toprakkilic.com /srv/www/toprakkilic.com
 
-# firewall setup and init
+# security config
+
+## firewall
 ufw default deny incoming
 ufw default allow outgoing
 ufw allow 22/tcp
@@ -35,35 +38,25 @@ ufw allow 443
 ufw allow 53
 ufw --force enable
 
-# ssh
-
-## install ssh pubkey
+## ssh
 install -d -m 700 -o admin -g admin /home/admin/.ssh
 echo "$PUBKEY" > /home/admin/.ssh/authorized_keys
 chown admin:admin /home/admin/.ssh/authorized_keys
 chmod 600 /home/admin/.ssh/authorized_keys
-
-## ssh hardening
-cat > /etc/ssh/sshd_config.d/00-hardening.conf <<'EOF'
-PasswordAuthentication no
-PermitRootLogin no
-KbdInteractiveAuthentication no
-AllowUsers admin
-EOF
+install -o root -g admin -m 640 /srv/www/toprakkilic.com/config/00-hardening.conf /etc/ssh/sshd_config.d/00-hardening.conf
 /usr/sbin/sshd -t
 systemctl restart ssh
 fail2ban-client status sshd
 
 # website init
-[ -d /srv/www/toprakkilic.com/.git ] || git clone https://github.com/topklc/toprakkilic.com /srv/www/toprakkilic.com
 chown -R admin:admin /srv/www/toprakkilic.com
-cp /srv/www/toprakkilic.com/config/Caddyfile /etc/caddy/Caddyfile
+install -m 640 -g admin /srv/www/toprakkilic.com/config/Caddyfile /etc/caddy/Caddyfile
 systemctl enable --now caddy fail2ban
 systemctl reload caddy
 
 # dns
 
-## tsig key for zone transfers
+## tsig keygen for zone transfers
 if [ ! -s /etc/knot/keys.conf ]; then
 install -m 640 -g knot /dev/null /etc/knot/keys.conf
 printf 'key:\n  - id: xfer-key\n    algorithm: hmac-sha256\n    secret: "%s"\n' \
